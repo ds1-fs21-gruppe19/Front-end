cdabb13930aef991527654c0e4829c7a38cb9dd5
TobiasRothlin <tobias@rothlin.com>
2021-03-23 15:36:00 +0100

    Email verification regex

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -52,11 +52,18 @@ class Login extends React.Component {
 
     emailHasChanged(e)
     {
-      var regex1 = /@/;
-      var regex2 = /./;
-      var result = regex2.test(e.target.value) ;
-      console.log(result);
+      let regex1 = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+      let validEmail = regex1.test(e.target.value);
+      if(validEmail)
+      {
         this.setState({email : e.target.value});
+        console.log("Valid Email enterd");
+      }
+      else
+      {
+
+      }
+      
     }
 
     passwordHasChanged(e)
